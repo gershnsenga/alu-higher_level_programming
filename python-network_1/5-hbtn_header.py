3232827384a2f974d6dc383b0795aaf0662c063c
gershnsenga <156787203+gershnsenga@users.noreply.github.com>
2024-03-24 19:19:36 +0200

    headers

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """__summary__
 - Write a Python script that takes in a URL,
@@ -9,6 +8,5 @@ import sys
 import requests
 
 if __name__ == '__main__':
-        reqs = requests.get(sys.argv[1])
-            print(reqs.headers.get("X-Request-Id"))
-
+    reqs = requests.get(sys.argv[1])
+    print(reqs.headers.get("X-Request-Id"))
