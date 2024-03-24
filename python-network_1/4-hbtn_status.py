d7d1f359536b169b3acdc530cdfa5556f90397df
gershnsenga <156787203+gershnsenga@users.noreply.github.com>
2024-03-24 21:30:19 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""_summary_
+"""__summary__
 - Write a Python script that
 - fetches https://intranet.hbtn.io/status.
 """
 import requests
 
-url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-else:
-    print("Error: Unable to fetch data from the URL. Status code:", response.status_code)
-
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
