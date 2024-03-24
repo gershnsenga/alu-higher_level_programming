fab413db3a604233580215fc23abe3f8bdce87f2
gershnsenga <156787203+gershnsenga@users.noreply.github.com>
2024-03-24 19:19:53 +0200

    email posting

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """__summary__
-- Write a Python script that takes in a URL,
-- sends a request to the URL and displays the value of the
-- variable X-Request-Id in the response header
+- Write a Python script that takes in a URL and an email address,
+- sends a POST request to the passed URL with the email as a parameter,
+- and finally displays the body of the response.
 """
 import sys
 import requests
 
 if __name__ == '__main__':
-    reqs = requests.get(sys.argv[1])
-    print(reqs.headers.get("X-Request-Id"))
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    reqs = requests.post(url, data=value)
+    print(reqs.text)
