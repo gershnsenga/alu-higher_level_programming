1fa2fe5f7a165c7c5bdebaaffaa902b051847939
gershnsenga <156787203+gershnsenga@users.noreply.github.com>
2024-03-24 21:19:38 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format("OK" if len(res.text) > 100 else res.text)
+    print("\t- content: {}".format(res.text))
